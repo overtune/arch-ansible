cd54fff910a13e4c1eac2341c02f9110801be5a2
Johan Runesson <johan.runesson@consid.se>
2019-09-06 13:25:44 +0200

    Adding dotfiles handling.

--- roles/dotfiles/scripts/setup_dotfiles.sh
+++ roles/dotfiles/scripts/setup_dotfiles.sh
@@ -1,1 +1,4 @@
-echo $HOME 
+cd $HOME 
+git clone --bare https://github.com/overtune/dotfiles.git $HOME/.dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+config checkout
